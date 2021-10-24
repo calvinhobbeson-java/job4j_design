6ae289e9424bdd0fc02bb551d5751a4a4eb5b5ff
Ruviel21 <calvinhobbeson.java@gmail.com>
2021-10-24 16:33:01 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/Matrix.java
+++ src/main/java/ru/job4j/io/Matrix.java
@@ -1,17 +1,15 @@
 package ru.job4j.io;
 
 import java.io.FileOutputStream;
-import java.nio.charset.StandardCharsets;
 
 public class Matrix {
     public void multiple(int size) {
         try (FileOutputStream out = new FileOutputStream("multiTable.txt")) {
             for (int i = 1; i <= size; i++) {
                 for (int j = 1; j <= size; j++) {
-                    String result = String.valueOf(i * j);
+                    String result = i * j + " ";
                     out.write(result.getBytes());
-                    out.write(System.lineSeparator().getBytes());
-                }
+                }  out.write(System.lineSeparator().getBytes());
             }
         } catch (Exception e) {
             e.printStackTrace();
