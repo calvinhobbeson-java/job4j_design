e5262617aca08014eceedac2ab5f2167a162fe5a
Ruviel21 <calvinhobbeson.java@gmail.com>
2022-03-20 22:50:47 +0300

    1. Inner join [#373617]

---
+++ users_wallets.sql
@@ -0,0 +1,32 @@
+create table wallet (
+	id serial primary key,
+	name text,
+	number int
+);
+
+create table users (
+	id serial primary key,
+	name text,
+	wallet_id int references wallet(id) unique
+);
+
+insert into wallet (name, number) values ('advCash', 12345);
+insert into wallet (name, number) values ('pgwPingwi', 54321);
+insert into wallet (name, number) values ('Jeton', 33456);
+
+insert into users (name, wallet_id) values ('Oleg', 1);
+insert into users (name, wallet_id) values ('Grigory', 2);
+insert into users (name, wallet_id) values ('Vasily', 3);
+insert into users (name) values ('Nikolaj');
+
+select u.name, w.name, w.number 
+from users as u join wallet as w 
+on u.wallet_id = w.number;
+
+select u.name as Имя, w.name as Название_кошелька, w.number as Номер_кошелька 
+from users as u join wallet as w 
+on w.name like 'adv%' and u.wallet_id = w.id;
+
+select u.name, w.name, w.number 
+from users as u join wallet as w 
+on u.name like 'Ol%' or u.name like 'Vas%';
