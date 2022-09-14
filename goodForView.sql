01e8261bc7239537368b6a57ed036d77191a332f
r.pakhomov <calvinhobbeson.java@gmail.com>
2022-09-14 21:32:05 +0600

    1. Представления [#504792]

--- goodForView.sql
+++ goodForView.sql
@@ -1,4 +1,47 @@
-select s.name, count(a.name), a.name, b.name from students as s
+create table students (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into students (name) values ('Иван Иванов');
+insert into students (name) values ('Петр Петров');
+
+create table authors (
+    id serial primary key,
+    name varchar(50)
+);
+
+insert into authors (name) values ('Александр Пушкин');
+insert into authors (name) values ('Николай Гоголь');
+
+create table books (
+    id serial primary key,
+    name varchar(200),
+    author_id integer references authors(id)
+);
+
+insert into books (name, author_id) values ('Евгений Онегин', 1);
+insert into books (name, author_id) values ('Капитанская дочка', 1);
+insert into books (name, author_id) values ('Дубровский', 1);
+insert into books (name, author_id) values ('Мертвые души', 2);
+insert into books (name, author_id) values ('Вий', 2);
+
+create table orders (
+    id serial primary key,
+    active boolean default true,
+    book_id integer references books(id),
+    student_id integer references students(id)
+);
+
+insert into orders (book_id, student_id) values (1, 1);
+insert into orders (book_id, student_id) values (3, 1);
+insert into orders (book_id, student_id) values (5, 2);
+insert into orders (book_id, student_id) values (4, 1);
+insert into orders (book_id, student_id) values (2, 2);
+
+
+create view show_me_random_stuff_that_i_guess as
+select s.name as student, count(a.name), a.name as author, b.name as book from students as s
     join orders o on s.id = o.student_id
     join books b on o.book_id = b.id
     join authors a on b.author_id = a.id
@@ -6,3 +49,5 @@ select s.name, count(a.name), a.name, b.name from students as s
 	and b.name like '%н%'
 	or  s.name like '%а%'
     group by (s.name, a.name, b.name) having count(a.name) <= 2;
+
+    select * from show_me_random_stuff_that_i_guess;
