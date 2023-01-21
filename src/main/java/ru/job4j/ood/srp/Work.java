41054b437cda333a71c5a5838d45bf1ece977efd
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-01-21 14:54:07 +0700

    0. Принцип единственной ответственности [#4913]

--- src/main/java/ru/job4j/ood/srp/Work.java
+++ src/main/java/ru/job4j/ood/srp/Work.java
@@ -1,11 +1,4 @@
 package ru.job4j.ood.srp;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 public interface Work {
-    String genre = new String();
-    List<String> tags = new ArrayList<>();
-    Date releasedTime = new Date();
 }
