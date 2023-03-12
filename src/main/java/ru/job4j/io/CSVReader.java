65a6f7401b506e348aa37df079cbcd46a58cc7ba
r.pakhomov <calvinhobbeson.java@gmail.com>
2023-03-12 21:01:46 +0700

    17. Scanner [#504791]

--- src/main/java/ru/job4j/io/CSVReader.java
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -1,16 +1,36 @@
-/*
 package ru.job4j.io;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.StringJoiner;
 
 public class CSVReader {
+    /**
+     * Записываем ключи, читаем  данные,  записываем  результат
+     * @param argsName
+     * @throws Exception
+     */
     public static void handle(ArgsName argsName) throws Exception {
-
+        String path  = argsName.get("path");
+        String delimiter = argsName.get("delimiter");
+        String out = argsName.get("out");
+        String filter = argsName.get("filter");
+        List<String> data = readFile(path);
+        writeData(path, filter, out, delimiter);
     }
+    private static List readFile(String path)  {
+        List<String> data = new ArrayList<>();
+        Scanner scanner = new Scanner(path);
+        while(scanner.hasNext()) {
+            data.add(scanner.nextLine());
+        }
+        return data;
+    }
+    private static writeData()
+
+
 }
-*/
---
+++ src/test/java/ru/job4j/ood/srp/report/AccountantsReportEngineTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.ood.srp.report;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.ood.srp.currency.Currency;
+import ru.job4j.ood.srp.currency.CurrencyConverter;
+import ru.job4j.ood.srp.currency.InMemoryCurrencyConverter;
+import ru.job4j.ood.srp.formatter.DateTimeParser;
+import ru.job4j.ood.srp.formatter.ReportDateTimeParser;
+import ru.job4j.ood.srp.model.Employee;
+import ru.job4j.ood.srp.store.MemStore;
+
+import java.util.Calendar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccountantsReportEngineTest {
+
+    @Test
+    public void whenAccountantsGenerated() {
+        MemStore store = new MemStore();
+        Calendar now = Calendar.getInstance();
+        Employee worker = new Employee("Ivan", now, now, 100);
+        DateTimeParser<Calendar> parser = new ReportDateTimeParser();
+        InMemoryCurrencyConverter converter = new InMemoryCurrencyConverter();
+        store.add(worker);
+        Report engine = new ReportEngine(store, parser);
+        StringBuilder expect = new StringBuilder()
+                .append("Name; Hired; Fired; Salary;")
+                .append(System.lineSeparator())
+                .append(worker.getName()).append(" ")
+                .append(parser.parse(worker.getHired())).append(" ")
+                .append(parser.parse(worker.getFired())).append(" ")
+                .append(converter.convert(Currency.RUB, 25000, Currency.EUR))
+                .append(System.lineSeparator());
+        assertThat(engine.generate(em -> true)).isEqualTo(expect.toString());
+    }
+}
